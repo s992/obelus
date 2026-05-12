e86c55c3ed7ebc5255ed2523d10561cc624c284b
Sean Walsh <sean.walsh87@gmail.com>
2026-05-11 19:32:44 -0700

    stop refetch

--- packages/client/src/client/client.ts
+++ packages/client/src/client/client.ts
@@ -8,6 +8,7 @@ export function getQueryClient() {
       defaultOptions: {
         queries: {
           staleTime: 60 * 1000,
+          refetchOnWindowFocus: false,
         },
       },
     });
