b0bbfd35744f61bacc311df38cb8d2838b486ed8
Sean Walsh <sean.walsh87@gmail.com>
2026-05-29 08:31:34 -0700

    tab focus state

--- packages/client/src/components/Tabs/tabs.css.ts
+++ packages/client/src/components/Tabs/tabs.css.ts
@@ -13,12 +13,13 @@ export const tabList = style([
 export const tab = style([
   typography.body,
   {
+    position: 'relative',
     display: 'inline-flex',
     alignItems: 'baseline',
     gap: vars.space.s2,
     borderBottom: `1px solid transparent`,
     marginBottom: -1,
-    padding: `${vars.space.s3} 0`,
+    padding: `${vars.space.s3} ${vars.space.s2}`,
     cursor: 'pointer',
     color: vars.color.ink3,
     textDecoration: 'none',
@@ -27,6 +28,27 @@ export const tab = style([
       '&[data-selected=true]': {
         color: vars.color.ink,
         borderBottomColor: vars.color.ink,
+      },
+      '&::after, &::before': {
+        fontFamily: vars.font.mono,
+        color: vars.color.ink,
+        position: 'absolute',
+        top: '48%',
+        transform: 'translateY(-50%)',
+        transition: 'opacity .15s ease',
+        pointerEvents: 'none',
+        opacity: 0,
+      },
+      '&::before': {
+        content: '[',
+        left: -2,
+      },
+      '&::after': {
+        content: ']',
+        right: -2,
+      },
+      '&[data-focus-visible=true]::before, &[data-focus-visible=true]::after': {
+        opacity: 1,
       },
     },
   },
